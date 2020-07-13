24da68f101fd54d5a239494d1fa7f7b61cdb217f
GaoFuq <1164842895@qq.com>
2020-07-13 12:52:12 +0800

    init

---
+++ app/src/main/java/com/gfq/umenglib/Config.java
@@ -0,0 +1,15 @@
+package com.gfq.umenglib;
+
+/**
+ * @created GaoFuq
+ * @Date 2020/7/9 11:58
+ * @Descaption
+ */
+public class Config {
+    //友盟AppKey
+    public static String AppKey = "5f0695d6978eea082dbd9e47";
+    public static String Umeng_Message_Secret = "da0c84149dbeb57cf16cffedaf01a72f";
+    public static String App_Master_Secret = "cwptesyyxyqrssbtwlo4qcms2zggf1y5";
+
+
+}
---
+++ app/src/main/java/com/gfq/umenglib/UmengLib.java
@@ -0,0 +1,49 @@
+package com.gfq.umenglib;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+
+/**
+ * @created GaoFuq
+ * @Date 2020/7/13 11:51
+ * @Descaption
+ */
+public class UmengLib {
+    private static final String TAG = "UmengLib";
+
+    private static class SingletonClassInstance {
+        private static final UmengLib instance = new UmengLib();
+    }
+
+    private UmengLib() {
+    }
+
+    public static UmengLib getInstance() {
+        return SingletonClassInstance.instance;
+    }
+
+
+    public void init(Context appContext, String appKey, String Umeng_Message_Secret) {
+        UMConfigure.init(appContext, appKey, "Umeng", UMConfigure.DEVICE_TYPE_PHONE, Umeng_Message_Secret);
+//获取消息推送代理示例
+        PushAgent mPushAgent = PushAgent.getInstance(appContext);
+//        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SERVER); //服务端控制声音
+        //注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
+                Log.e(TAG, "注册成功：deviceToken：-------->  " + deviceToken);
+            }
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.e(TAG, "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+            }
+        });
+
+    }
+}
